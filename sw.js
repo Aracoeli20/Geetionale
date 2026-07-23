193e2933273844365b7d82eb6f65656c87225dee
Aracoeli20 <Aracoeli20contatti@gmail.com>
2026-07-23 20:35:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='aracoeli-v23';
+const C='aracoeli-v24';
 const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==C?caches.delete(k):null))).then(()=>self.clients.claim()))});
